2cad60dd09dfb987f6aeee698b0c123d2a9798a6
JulienLeotier <julien.leotier@gmail.com>
2020-10-31 14:23:33 +0100

    Updates

---
+++ precache-manifest.731af812a7f481a3d1ec3f26482f5b98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91cab445e9612f21e62f81fe5b511971",
+    "url": "/playlist/index.html"
+  },
+  {
+    "revision": "b2f150cbe90629ea28d3",
+    "url": "/playlist/static/css/main.c9dea9a1.chunk.css"
+  },
+  {
+    "revision": "91f3a361ea18826f882e",
+    "url": "/playlist/static/js/2.383119b7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/playlist/static/js/2.383119b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f150cbe90629ea28d3",
+    "url": "/playlist/static/js/main.da76c425.chunk.js"
+  },
+  {
+    "revision": "4098689072fc3be9a5c3",
+    "url": "/playlist/static/js/runtime-main.53bc9397.js"
+  }
+]);
